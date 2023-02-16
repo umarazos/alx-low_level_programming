75bfdfa5fe5c802f82f630adbbaf8f1a21841b3c
umarazos <umarazos@icloud.com>
2023-02-16 15:20:57 -0800

    A C program that prints result using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a a string
+ *
+ * Return: Returns 0 on success
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
