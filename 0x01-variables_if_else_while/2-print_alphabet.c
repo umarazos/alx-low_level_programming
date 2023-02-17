00206387d89b15c6efb969e097591ab21586da27
umarazos <umarazos@icloud.com>
2023-02-17 12:24:48 -0800

    A program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabets a-z
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
