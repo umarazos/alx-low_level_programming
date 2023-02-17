23617599cd6d14cd541fe43fdf513939db5080c6
umarazos <umarazos@icloud.com>
2023-02-17 03:39:37 -0800

    A C program that prints a message to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
