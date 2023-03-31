9f44e914cc46f15411a779afa3150b4758ed1b76
umarazos <umarazos@icloud.com>
2023-03-30 18:49:10 -0700

    A function that runs before that main.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - invokes before the main
+ */
+
+void __attribute__ ((constructor)) first(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
